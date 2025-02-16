16cde025f6ad49806dc2cb78b6742d0240d58afa
surendra-sk <114029428+surendra-sk@users.noreply.github.com>
2025-02-16 13:56:08 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import  axios  from "axios";
 
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://tradbite-backend.onrender.com";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
